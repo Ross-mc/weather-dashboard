c34ddf917d992158329c71a0193aa0493c63410e
Ross McIntyre <rossmci90@hotmail.co.uk>
2020-11-25 12:59:14 +0000

    Added logic for getting all the day's forecasts
    An Array is created with the forecast for each day. Still hard coded to London

--- Assets/weatherscript.js
+++ Assets/weatherscript.js
@@ -2,29 +2,53 @@ $(function(){
 
     const APIKey = "60f60127e3114e968bdfe2591489ab29";
 
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=london&cnt=5&appid=" + APIKey;
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=london&appid=" + APIKey;
+
+    class weatherObj {
+      constructor(day, temperature, weather, icon, humidity, windspeed){
+        this.day = day;
+        this.temperature = temperature;
+        this.weather = weather;
+        this.icon = icon;
+        this.humidity = humidity;
+        this.windspeed = windspeed
+      }
+    };
+
+    const weatherArr = [];
 
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response) {
-      console.log(response)
-      var today = response.list[0]
+      // the response includes an array called list. Each element is the next 3 hour block
+      // we handle today seperately because we need to chain a uv request and because we use whatever the current time is
+
+      var dayArr = response.list;
+      var today = dayArr[0];
+
 
       var tempResponse = (parseInt(today.main.temp)- 273.15).toFixed(2);
-      console.log(tempResponse);
 
       var weatherResponse = today.weather[0].description;
-      console.log(weatherResponse);
 
       var iconResponse = today.weather[0].icon;
-      console.log(iconResponse)
 
       var humidityResponse = today.main.humidity;
-      console.log(humidityResponse);
 
       var windResponse = today.wind.speed;
-      console.log(windResponse);
+
+      var todayObj = new weatherObj(
+        "Now",
+        tempResponse,
+        weatherResponse,
+        iconResponse,
+        humidityResponse,
+        windResponse
+      );
+
+      weatherArr.push(todayObj);
+
 
 
       var lat = response.city.coord.lat;
@@ -39,8 +63,46 @@ $(function(){
           method: "GET"
       }).then(function(response){
           uvResponse = response.value
-          console.log(uvResponse);
-      })
+
+          weatherArr[0].uvIndex = uvResponse;
+
+      });
+
+      var currentDate = today.dt_txt;
+
+      // for loop for the remaining days
+      for (let i = 1; i<dayArr.length; i++){
+        var day = dayArr[i];
+        var date = day.dt_txt;
+
+        //to ensure we do not repeat today's forecast
+        if (date === currentDate){
+          break;
+        }
+        //we are going to use 15:00 as the forecast temp
+        if (date.endsWith("15:00:00")){
+          date = date.substr(0, 10);
+          var dayTemp = (parseInt(day.main.temp)-273.15).toFixed(1);
+          var dayWeather = day.weather[0].description;
+          var dayIcon = day.weather[0].icon;
+          var dayHumidity = day.main.humidity;
+          var dayWind = day.wind.speed;
+  
+          var dayObj = new weatherObj(
+            date,
+            dayTemp,
+            dayWeather,
+            dayIcon,
+            dayHumidity,
+            dayWind
+          );
+          weatherArr.push(dayObj);
+        }
+
+      }
+
+      console.log(weatherArr)
+
 
 
     })
